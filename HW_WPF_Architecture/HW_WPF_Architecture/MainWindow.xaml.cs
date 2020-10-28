5365a14fc3bd497ae51ac40a209460db65272c9a
Gulzada98 <71434804+Gulzada98@users.noreply.github.com>
2020-10-28 23:12:35 +0600

    Add files via upload

---
+++ HW_WPF_Architecture/HW_WPF_Architecture/MainWindow.xaml.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Data;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace HW_WPF_Architecture
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            foreach (UIElement el in mainRoot.Children)
+            {
+                if (el is Button)
+                {
+                    ((Button)el).Click += Button_Click;
+                }
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string str = (string)((Button)e.OriginalSource).Content;
+
+            if (str == "CE")
+            {
+                textLabel.Text = "";
+            }
+            else if (str == "=")
+            {
+                string value = new DataTable().Compute(textLabel.Text, null).ToString();
+                textLabel.Text = value;
+            }
+            else if (str == "C")
+            {
+                str = (string)(textLabel.Text);
+                textLabel.Text = str.Remove(str.Length - 1);
+            }
+            else
+            {
+                textLabel.Text += str;
+            }
+
+        }
+
+        //public MainWindow()
+        //{
+        //    InitializeComponent();
+
+
+        //}
+
+    }
+}
